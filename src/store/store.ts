86f2fc12267070ce7d0e92553b27c6d292840caa
EugeneKGen <eugene_korobkin@mail.ru>
2024-04-23 23:47:17 +0300

    chore: cleaning useless code

---
+++ src/store/store.ts
@@ -0,0 +1,18 @@
+import type { Action, ThunkAction } from "@reduxjs/toolkit"
+import { combineSlices, configureStore } from "@reduxjs/toolkit"
+
+const rootReducer = combineSlices()
+
+export type RootState = ReturnType<typeof rootReducer>
+
+export const store = configureStore({
+  reducer: {},
+})
+
+export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ThunkReturnType = void> = ThunkAction<
+  ThunkReturnType,
+  RootState,
+  unknown,
+  Action
+>
